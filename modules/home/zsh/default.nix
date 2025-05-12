5f4321016fc42101fed18e6c8816bfbad72439d6
Brandon Talbot <bjtal91@gmail.com>
2025-05-11 20:55:03 -0500

    add fish integration to zoxide

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -1,5 +1,4 @@
 {
-  profile,
   pkgs,
   lib,
   ...
@@ -44,8 +43,8 @@
       sv = "sudo nvim";
       v = "nvim";
       c = "clear";
-      fr = "nh os switch --hostname ${profile}";
-      fu = "nh os switch --hostname ${profile} --update";
+      fr = "nh os switch --hostname default";
+      fu = "nh os switch --hostname default --update";
       zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/releases/latest/download/install-zaneyos.sh)";
       ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       cat = "bat";
