d34673e049090e4ec96261651832d6751da7d1b4
Tyler Kelley <tylerzanekelley@gmail.com>
2025-05-13 00:08:38 -0500

    Updating OS name and version

--- modules/home/fastfetch/default.nix
+++ modules/home/fastfetch/default.nix
@@ -26,7 +26,7 @@
         "break"
         {
           type = "os";
-          key = "OS - ZaneyOS v2.3";
+          key = "OS - DarkJaguar-NixOS V0.1";
           keyColor = "31";
         }
         {
