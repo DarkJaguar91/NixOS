1b2d918beb5b9b41d06b879d71599e36ba821835
Brandon Talbot <bjtal91@gmail.com>
2025-09-14 00:31:49 -0700

    Some nvim setup

--- modules/core/nvim.nix
+++ modules/core/nvim.nix
@@ -21,6 +21,9 @@
     gcc # Needed for treesitter
   ];
 
+  # Because we are using Mason, we need a nix linker for mason installed lsps
+  programs.nix-ld.enable = true;
+
   environment.etc."tmpfiles.d/home-${usr.login}-nvim.conf".text = ''
     L+    /home/${usr.login}/.config/nvim                   -    ${usr.login}    -     -           /home/${usr.login}/.config/nixos/assets/dots/nvim
   '';
