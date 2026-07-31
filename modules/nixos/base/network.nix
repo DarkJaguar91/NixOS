d4e76cea8e557e515d9e3a55e3b360bbabd851d1
Brandon Talbot <bjtal91@gmail.com>
2026-07-31 00:39:30 -0700

    Set wifi regdom to CA and disable powersave

--- modules/nixos/base/network.nix
+++ modules/nixos/base/network.nix
@@ -1,5 +1,12 @@
 {
   flake.modules.nixos.base = {
     networking.networkmanager.enable = true;
+
+    # Set wifi regulatory domain (fixes "regulatory prevented using AP
+    # config, downgraded" and degraded 6GHz links)
+    boot.kernelParams = [ "cfg80211.ieee80211_regdom=CA" ];
+
+    # Disable wifi powersaving (fixes latency spikes/jitter)
+    networking.networkmanager.settings.connection."wifi.powersave" = 2;
   };
 }
